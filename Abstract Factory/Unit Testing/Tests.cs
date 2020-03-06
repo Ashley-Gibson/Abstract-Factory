222a4938aa19296940d48a377a9c2111ce447303
ashleydgibson <ashley_gibson@ajg.com>
2020-03-06 15:37:30 +0000

    task/AF-102 - Initial commit for Unit Testing suite

---
+++ Abstract Factory/Unit Testing/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
